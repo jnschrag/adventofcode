dacdcbfdeb265470357fd8d1256b976178704207
jnschrag <jnschrag@gmail.com>
2018-12-02 14:07:23 -0500

    Day 2, part 2

---
+++ 2018/2/part2.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require('path');
+const text = fs.readFileSync(path.join(__dirname) + '/input.txt', 'utf8');
+const input = text.split("\n")
+
+let check = true
+
+for (let line of input) {
+  const comparisonLines = input.filter(d => d !== line)
+
+  for (let entry of comparisonLines) {
+    let result = findFirstDiffPos(line, entry)
+    if (result.numDiff === 1) {
+      console.log(line, entry)
+      let string = line.slice(0, result.posDiff) + line.slice(result.posDiff + 1)
+      console.log(string)
+      check = false
+      break
+    }
+  }
+  if (!check) break
+}
+
+function findFirstDiffPos(string1, string2) {
+  if (string1 === string2) {
+    return
+  }
+
+  const length = Math.max(string1.length, string2.length)
+  let numDiff = 0
+  let posDiff = 0
+
+  for (let i = 0; i <= length; i++) {
+    if (string1[i] !== string2[i]) {
+      numDiff++
+      posDiff = i
+    }
+  }
+  return {string1: string1, string2: string2, numDiff: numDiff, posDiff: posDiff}
+}
