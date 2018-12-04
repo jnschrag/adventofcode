0780ef78e3e596e30955bec1e4d5b98d80e8c24d
jnschrag <jnschrag@gmail.com>
2018-12-03 23:12:46 -0500

    Day 3, part 2

--- 2018/3/part2.js
+++ 2018/3/part2.js
@@ -1,26 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 const text = fs.readFileSync(path.join(__dirname) + "/input.txt", "utf8");
-const input = text.split("\n").map(line =>
-  line
-    .split("@")[1]
+const input = text.split("\n").map(line => {
+  let parts = line.split("@")
+  let coords = parts[1]
     .split(":")
     .map(d => d.trim())
+  return {id: parts[0].trim(), info: coords, coords: []}
+}
 );
 
 let coords = {};
 
-input.forEach((line, i) => {
-  const start = line[0].split(",");
+input.forEach(line => {
+  const start = line.info[0].split(",");
   const leftStart = +start[0];
   const topStart = +start[1];
-  const size = line[1].split("x");
+  const size = line.info[1].split("x");
   const width = +size[0] + leftStart;
   const height = +size[1] + topStart;
 
   for (var x = leftStart; x < width; x++) {
     for (var y = topStart; y < height; y++) {
       let pos = x + "," + y;
+      line.coords.push(pos)
       if (coords[pos]) {
         coords[pos] = coords[pos] + 1;
       } else {
@@ -30,7 +33,10 @@ input.forEach((line, i) => {
   }
 });
 
-let result = Object.values(coords).filter(coord => coord > 1).length;
+for (let line of input) {
+  if (line.coords.every(d => coords[d] === 1)) {
+    console.log(line.id)
+    return;
+  }
+}
 
-console.log(result);
-console.log(coords);
