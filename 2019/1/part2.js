13e9ab980b4077696cfa04857d9c7798552c5c90
Jacque Schrag <jnschrag@gmail.com>
2019-12-01 14:29:26 -0500

    Add Day 1 part 2 as separate file

--- 2019/1/part2.js
+++ 2019/1/part2.js
@@ -3,9 +3,20 @@ const path = require("path");
 const text = fs.readFileSync(path.join(__dirname) + "/input.txt", "utf8");
 let output = text
   .split("\n")
-  .map(d => calcFuelPerModule(d))
+  .map(d => calcFuel(d))
   .reduce((acc, curr) => acc + curr, 0);
 console.log(output);
+
+function calcFuel(mass) {
+  let fuel = calcFuelPerModule(mass);
+  if (fuel > 0) {
+    let fuelFuel = calcFuel(fuel);
+    if (fuelFuel > 0) {
+      fuel += calcFuel(fuel);
+    }
+  }
+  return fuel;
+}
 
 function calcFuelPerModule(mass) {
   mass = +mass;
